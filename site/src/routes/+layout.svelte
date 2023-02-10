eb4ace227bbe8f7026c202a393a19f316f6ca5b5
roypriyanshu02 <62667551+roypriyanshu02@users.noreply.github.com>
2023-02-10 21:34:06 +0530

    Implement basic layout
    Co-authored-by: rakeshid03 <67048804+rakeshid03@users.noreply.github.com>

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import Header from '$lib/components/header.svelte';
+	import BackToTop from '$lib/components/back-to-top.svelte';
+</script>
+
+<Header />
+<main>
+	<slot />
+	<BackToTop />
+</main>
