d3c3b085baaf8ceee2d5ea9861f640faf6032671
roypriyanshu02 <62667551+roypriyanshu02@users.noreply.github.com>
2023-03-07 23:23:08 +0530

    Add meta tags and favicon
    Co-authored-by: rakeshid03 <67048804+rakeshid03@users.noreply.github.com>

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -57,7 +57,7 @@
 		<IntersectionObserver let:intersecting once>
 			{#if intersecting}
 				<Card
-					screenshot={`https://raw.githubusercontent.com/roypriyanshu02/impressive-profile-readmes/master/screenshot/${profile.username}.webp`}
+					screenshot={`https://raw.githubusercontent.com/roypriyanshu02/impressive-profile-readmes/main/screenshots/${profile.username.toLowerCase()}.webp`}
 					username={profile.username}
 					category={profile.category}
 					starCount={profile.starCount}
@@ -67,3 +67,39 @@
 		</IntersectionObserver>
 	{/each}
 </CardSection>
+
+<svelte:head>
+	<meta name="theme-color" content="var(--color-background)" />
+	<title>Impressive Profile Readmes</title>
+	<meta
+		name="description"
+		content="Get inspired to make your GitHub Profile stand out! Check out our gallery of impressive READMEs and take your profile to the next level."
+	/>
+	<link rel="canonical" href="https://roypriyanshu02.github.io/impressive-profile-readmes/" />
+	<meta name="author" content="Contributors" />
+	<meta name="robots" content="index, follow" />
+	<!-- Meta Tags For Open Graph / Facebook -->
+	<meta property="og:type" content="Website" />
+	<meta property="og:title" content="Impressive Profile Readmes" />
+	<meta
+		property="og:description"
+		content="Get inspired to make your GitHub Profile stand out! Check out our gallery of impressive READMEs and take your profile to the next level."
+	/>
+	<meta property="og:url" content="https://roypriyanshu02.github.io/impressive-profile-readmes/" />
+	<meta
+		property="og:image"
+		content="https://roypriyanshu02.github.io/impressive-profile-readmes/screenshot.webp"
+	/>
+	<!-- Meta Tags For Twitter -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Impressive Profile Readmes" />
+	<meta
+		name="twitter:description"
+		content="Get inspired to make your GitHub profile stand out! Browse our collection of impressive READMEs and take your profile to the next level."
+	/>
+	<meta name="twitter:url" content="https://roypriyanshu02.github.io/impressive-profile-readmes/" />
+	<meta
+		name="twitter:image"
+		content="https://roypriyanshu02.github.io/impressive-profile-readmes/screenshot.webp"
+	/>
+</svelte:head>
