143a559050b3e972d839ceec9bb2adc51b8c1c42
Priyanshu Roy <62667551+roypriyanshu02@users.noreply.github.com>
2026-07-02 21:14:00 +0530

    fix(deps): upgrade dev dependencies and migrate to flat eslint config

--- site/vite.config.js
+++ site/vite.config.js
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// Optimize dependency pre-bundling
+	optimizeDeps: {
+		include: ['svelte', '@sveltejs/kit'],
+		exclude: ['@sveltejs/kit/ssr']
+	}
 };
 
 export default config;
